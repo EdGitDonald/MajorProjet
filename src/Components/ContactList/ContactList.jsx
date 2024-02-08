4797333261ae5978d0899945052c6c288e469b79
Edward Donald <donald808@icloud.com>
2024-02-08 17:48:45 +0000

    Contact Widget adjusted

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -1,18 +1,27 @@
+// ContactList.jsx
+
 import React from 'react';
+import './ContactList.css';
+import { IoIosContacts } from "react-icons/io";
+import { AiOutlineMessage } from "react-icons/ai";
 
 function ContactList({ contacts, openMessageModal, handleRemoveContact }) {
   return (
     <div className='Contacts-list'>
       {contacts.map((contact, index) => (
         <div className='Contact' key={index}>
-          <img src={contact.image} alt={`${contact.name} avatar`} />
+          {contact.image ? (
+            <img src={contact.image} alt={`${contact.name} avatar`} />
+          ) : (
+            <IoIosContacts className="default-icon" />
+          )}
           <div>
             <p>{contact.name}</p>
           </div>
-          <button onClick={() => openMessageModal(contact)}>Msg</button>
-          <button>Voice memo</button>
-          <button>Camera</button>
-          <button onClick={() => handleRemoveContact(index)}>Remove</button>
+          <div className="button-row">
+            <button onClick={() => openMessageModal(contact)}><AiOutlineMessage /></button>
+            <button onClick={() => handleRemoveContact(index)}>Remove</button>
+          </div>
         </div>
       ))}
     </div>
@@ -20,3 +29,4 @@ function ContactList({ contacts, openMessageModal, handleRemoveContact }) {
 }
 
 export default ContactList;
+
