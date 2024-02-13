07334e2050a0f75eae3fa9b304a69c09a0eeb210
Edward Donald <donald808@icloud.com>
2024-02-13 16:49:25 +0000

    Adjustment and drag function

--- src/Components/Notifications/Notifications.jsx
+++ src/Components/Notifications/Notifications.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { TiDelete } from "react-icons/ti";
 import './Notifications.css';
 
 function Notifications({ draftedMessages, removeNotification, urgencyFilter, handleUrgencyFilterChange }) {
   const [selectedUrgency, setSelectedUrgency] = useState(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
+  const notificationsRef = useRef(null);
 
   const handleFlagClick = (urgency) => {
     setSelectedUrgency(urgency);
@@ -31,8 +34,36 @@ function Notifications({ draftedMessages, removeNotification, urgencyFilter, han
     }
   };
 
+  const handleMouseDown = (event) => {
+    setIsDragging(true);
+    const notificationsRect = notificationsRef.current.getBoundingClientRect();
+    setDragOffset({
+      x: event.clientX - notificationsRect.left,
+      y: event.clientY - notificationsRect.top
+    });
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleMouseMove = (event) => {
+    if (isDragging) {
+      const newX = event.clientX - dragOffset.x;
+      const newY = event.clientY - dragOffset.y;
+      notificationsRef.current.style.left = `${newX}px`;
+      notificationsRef.current.style.top = `${newY}px`;
+    }
+  };
+
   return (
-    <div className='Notifications'>
+    <div
+      ref={notificationsRef}
+      className='Notifications'
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+    >
       <div className='Notifications-container'>
         <h2>Notifications</h2>
         
@@ -53,10 +84,10 @@ function Notifications({ draftedMessages, removeNotification, urgencyFilter, han
             .map((message, index) => (
               <li key={index} className={`urgent-${message.urgency}`}>
                 <div className='message-header'>
-                <div className="urgency-box" style={{ backgroundColor: getUrgencyColor(message.urgency) }}></div>
-                <button onClick={() => handleRemoveNotification(index)}>
-                <TiDelete />
-                </button>
+                  <div className="urgency-box" style={{ backgroundColor: getUrgencyColor(message.urgency) }}></div>
+                  <button onClick={() => handleRemoveNotification(index)}>
+                    <TiDelete />
+                  </button>
                 </div>
                 <div className="message-content">
                   <p>From: {message.contact.name} : {message.message}</p>
@@ -70,4 +101,5 @@ function Notifications({ draftedMessages, removeNotification, urgencyFilter, han
 }
 
 export default Notifications;
+
 
